d6d158107d3770061d91a064dc76d9572c139e81
Laima Petrauskiene <laima@tinklai.net>
2021-11-13 17:49:58 +0200

    5:metodai veikia

---
+++ js/API.js
@@ -0,0 +1,26 @@
+const baseURL = 'http://localhost:3000';
+
+class API {
+    static fetchCars = (success, failure) => {
+        fetch(`${baseURL}/cars`)
+            .then(res => res.json())
+            .then(success)
+            .catch(failure)
+    }
+    static deleteCar = (id, success, failure) => {
+        fetch(`${baseURL}/cars/${id}`, {method: 'DELETE'})
+            .then(res => res.status === 200 ? success() : failure(res.status.text))
+            .catch(failure)
+    }
+}
+
+// API.fetchCars(
+//     console.log,
+//     console.error
+// )
+
+// API.deleteCar(
+//     "3",
+//     () =>console.log('Ištrinta sėkmingai'),
+//     console.error
+// )
