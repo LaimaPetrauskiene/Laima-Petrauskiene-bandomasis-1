4d5d46c3b61878d8f42555c6d54a51e699163da9
Laima Petrauskiene <laima@tinklai.net>
2021-11-13 18:45:52 +0200

    7:kortelių dėstymas

--- js/components/CarGridComponent.js
+++ js/components/CarGridComponent.js
@@ -14,6 +14,7 @@ class CarGridComponent {
     saveCars = (cars) => {
         this.state.cars = cars;
         this.state.loading = false;
+        this.htmlElement.className = 'row g-3';
 
         this.render();
     }
@@ -26,6 +27,13 @@ class CarGridComponent {
         this.render();
     }
 
+    wrapInColumn = (element) => {
+        const column = document.createElement('div');
+        column.className = 'col-12 col-sm-6 col-lg-3 col-xl-4';
+        column.appendChild(element);
+        return column;
+    }
+
     render = () => {
         const { loading, cars } = this.state;
         if (loading) {
@@ -34,7 +42,8 @@ class CarGridComponent {
             this.htmlElement.innerHTML = '';
             const carElements = cars
             .map(x => new CarCardComponent(x))
-            .map(x => x.htmlElement);
+            .map(x => x.htmlElement)
+            .map(this.wrapInColumn);
             this.htmlElement.append(...carElements);
             
         }else{
